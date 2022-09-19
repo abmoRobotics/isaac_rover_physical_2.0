91b6c52cc7e13917badee0cb84bf904efaa60dc0
“Anton <abmo19@student.aau.dk>
2022-09-19 12:21:17 -0100

    change scripts

--- Docker/ros_entrypoint.sh
+++ Docker/ros_entrypoint.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-set -e
+if [[ $1 == "config" ]]
+then
+	cd /root/exomy_ws/src/exomy/scripts
+	bash
+elif [[ $1 == "autostart" ]]
+then
+	source /opt/ros/foxy/setup.bash
+	#cd /root/exomy_ws
+	#catkin_make
+	#http-server src/exomy/gui -p 8000 &
 
-# setup ros environment
-source "/opt/ros/foxy/setup.bash"
-exec "$@"
+	#source devel/setup.bash
+	#roslaunch exomy exomy.launch
+
+	bash
+elif [[ $1 == "devel" ]]
+then
+	cd /home/rover_ws
+	source /opt/ros/foxy/setup.bash
+	# catkin_make
+	# source devel/setup.bash
+	bash
+else
+	bash
+fi
