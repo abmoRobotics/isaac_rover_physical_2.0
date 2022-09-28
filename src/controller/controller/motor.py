dff8c97e46e64e9f92fccf238a79f28772b63b10
“Anton <abmo19@student.aau.dk>
2022-09-28 14:51:41 +0000

    Started CANopen work

--- src/controller/controller/motor.py
+++ src/controller/controller/motor.py
@@ -1,6 +1,10 @@
+from turtle import right
 from unittest.mock import CallableMixin
 import rclpy
 from rclpy.node import Node
-import ros2controlcli
+#from ros2controlcli.api.
+import ros2_canopen.canopen_interfaces as can_interface
 
+msg = can_interface()
 
+msg.x 
