026babb94cecad582213af3514a91cee0d242847
“Anton <abmo19@student.aau.dk>
2022-10-03 14:39:46 +0000

    Update on Jetson.GPIO

--- src/controller/controller/motor.py
+++ src/controller/controller/motor.py
@@ -1,10 +1,10 @@
-from turtle import right
-from unittest.mock import CallableMixin
-import rclpy
-from rclpy.node import Node
-#from ros2controlcli.api.
-import ros2_canopen.canopen_interfaces as can_interface
+# from turtle import right
+# from unittest.mock import CallableMixin
+# import rclpy
+# from rclpy.node import Node
+# #from ros2controlcli.api.
+# #import ros2_canopen.canopen_interfaces as can_interface
+import Jetson.GPIO as GPIO 
+# #msg = can_interface()
 
-msg = can_interface()
-
-msg.x 
+# #msg.x 
