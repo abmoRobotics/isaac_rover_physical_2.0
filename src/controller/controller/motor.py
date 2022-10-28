affe842ab70555807c2c7057b39328aa22833703
abmoRobotics <abmo19@student.aau.dk>
2022-10-28 15:55:09 +0200

    3 motors works wihout auto setup

--- src/controller/controller/motor.py
+++ src/controller/controller/motor.py
@@ -33,33 +33,39 @@ class motor_subscriber(Node):
         global FL, FR, CL, CR, RL, RR, FL_EN, FR_EN, RL_EN, RR_EN
         FL, FR, CL, CR, RL, RR, FL_EN, FR_EN, RL_EN, RR_EN = range(0,10) 
 
-        ID = range(0,1)
+        ID = [0,1,2]
         self.mode_oper  =   []
         self.target_vl  =   []
         self.mode_disp  =   []
         self.status     =   []
         self.control    =   []
-        self.DeltaSpeed =   []
-        self.DeltaTime  =   []
-
+        self.actual_vl  =   [] 
 
         for id in ID:
-            self.node = network.add_node(id + 1, eds_path)
-            self.get_logger().info('NodeID: ' +str(id + 1) )
+            
+            self.node = network.add_node(id+1, eds_path)
+            node_id = id + 1
+            self.get_logger().info('NodeID: ' + str(node_id) )
             self.mode_oper.append(self.node.sdo['Modes of operation'])
             self.target_vl.append(self.node.sdo['vl target velocity'])
             self.mode_disp.append(self.node.sdo['Modes of operation display'])
             self.status.append(self.node.sdo['Statusword'])
             self.control.append(self.node.sdo['Controlword'])
-            self.DeltaSpeed.append(self.node.sdo['vl velocity acceleration']['DeltaSpeed'])
-            self.DeltaTime.append(self.node.sdo['vl velocity acceleration']['DeltaTime'])
+            self.actual_vl.append(self.node.sdo[0x6044])
+            
+            
+            self.node.sdo[0x2030].phys = 6              # pole pair = 6      step angle = 15
+            self.node.sdo[0x2031].phys  = 14000         # maximum permissible motor current in mA
+            self.node.sdo[0x6075].phys  = 5000          # rated current of the motor in mA 
+            self.node.sdo[0x6073].phys  = 15000         # maximum current in mA
+            self.node.sdo[0x3202].raw   = 0x00000040    # Motor type = BLDC
 
             # initilize the mode to velocity(2)
             self.mode_oper[id].phys = 0x02
 
             # Setting the acc
-            self.DeltaSpeed[id].phys = 2000
-            self.DeltaTime[id].phys = 10
+            self.node.sdo['vl velocity acceleration']['DeltaSpeed'].phys = 2000
+            self.node.sdo['vl velocity acceleration']['DeltaTime'].phys = 5
 
             # Initilize the motors
             if self.mode_disp[id].phys == 2:
@@ -84,13 +90,18 @@ class motor_subscriber(Node):
 
     def listener_callback(self, msg):
         
-        self.get_logger().info('Publishing: "%s"' % str(msg.motor_linear_vel) + ' Ang: ' + str(msg.motor_angular_vel)+ ' Mode: ' + str(msg.mode))
+        #self.get_logger().info('Publishing: "%s"' % str(msg.motor_linear_vel) + ' Ang: ' + str(msg.motor_angular_vel)+ ' Mode: ' + str(msg.mode))
         
         
+        self.target_vl[FR].phys = msg.motor_linear_vel * 2000
         self.target_vl[FL].phys = msg.motor_linear_vel * 2000
-
+        self.target_vl[CL].phys = msg.motor_linear_vel * 2000
+        self.get_logger().info('Node1 "%s"' % str(self.actual_vl[FL].phys) + ' Node2: "%s"' % str(self.actual_vl[FR].phys)+ ' Node3: "%s"' % str(self.actual_vl[CL].phys))
         if msg.power_off == 1:
-            self.control[FL].phys = 0 
+            self.control[FL].phys = 0
+            self.control[FR].phys = 0  
+            self.control[CL].phys = 0  
+            self.node.sdo[0x1011][0x05].phys = 0x64616F6C # Restart the drive 
             self.destroy_node()
             rclpy.shutdown()
         
